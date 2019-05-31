5b442a9f92dda7e1f298394d9d55fc3af4115158
mohamedazab <mohamedazab1996@gmail.com>
2019-05-31 16:26:38 +0200

    remove: comment

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import homePageView
-#dummy comment to test CI and the deployed image on docker hub
+
 urlpatterns = [
     path('', homePageView, name = 'home')
 ]

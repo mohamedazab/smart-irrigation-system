1931beadde47fb85408d2c8da95c6164529ce714
mohamedazab <mohamedazab1996@gmail.com>
2019-05-31 12:55:52 +0200

    test: dummy testcase

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+        
+    def test_one_plus_one_equals_two(self):
+            print("Method: test_one_plus_one_equals_two (dummy test).")
+            self.assertEqual(1 + 1, 2)

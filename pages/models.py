7be0141705f244fdfb1c59e934a19925a09a563a
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2019-07-20 21:22:58 +0200

    feat: add plant and user models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=50)
+    moisture_threshold = models.FloatField()
+    recommended_ph = models.FloatField()
+    recommended_temperature = models.FloatField()
+
+
+class User(models.Model):
+    premium = models.BooleanField()
+    email = models.EmailField()
+    password_salt = models.CharField(max_length=128)
+    field = models.ArrayReferenceField(
+        to=Plant,
+        on_delete=models.DO_NOTHING
+    )

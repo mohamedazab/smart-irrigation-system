6ff3ee4d20625f9f8f3e53c648c26be38d5a89ce
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2019-07-23 18:48:50 +0200

    feat: plant retrieve and small fixes

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import createPlant
+from .views import createPlant, retrievePlant
 
 urlpatterns = [
-    path('api/plant', createPlant, name='Create Plant')
+    path('api/plant', createPlant, name='Create Plant'),
+    path('api/plant/<str:id>', retrievePlant, name='Retrieve Plant')
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,20 +6,72 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 from .models import Plant, User
+from bson.objectid import ObjectId
 
 @csrf_exempt
-def createPlant(request):#plantJSON):
-    print("new request with: ", request.method)
-
-    if request.method != "POST" or request.body== None:
-        response  = HttpResponse('{"message": false}', content_type="application/json")
+def createPlant(request):
+    # Only listen to POST requests
+    if request.method != "POST" or request.body == None:
+        response  = HttpResponse(
+            '''{
+                "success": false, 
+                "message": "Only POST requests are allowed on this route"
+            }''',
+            content_type="application/json"
+        )
         response.status_code = 300
         return response
     
     body = json.loads(request.body)
-    Plant.objects.mongo_insert(body)
-    return HttpResponse('{"message": true}')
+    Plant.objects.mongo_insert(body)   
+    response = HttpResponse(
+        '''{
+            "success": true, 
+            "message": "Plant inserted successfully"
+        }''',
+        content_type="application/json"
+    )
+    response.status_code = 200
+    return response
+
 
 @csrf_exempt
-def retrievePlant(plantID):
-    return Plant.objects.mongo_find_one({'_id': plantID})
+def retrievePlant(request, id):
+    # Only listen to GET requests
+    if request.method != "GET":
+        response  = HttpResponse(
+            '''{
+                "success": false, 
+                "message": "Only GET requests are allowed on this route"
+            }''',
+            content_type="application/json"
+        )
+        response.status_code = 300
+        return response
+    
+    plant = Plant.objects.mongo_find_one({'_id': ObjectId(id)})
+    # Plant not found
+    if plant == None:
+        response  = HttpResponse(   
+            '''{
+                "success": false, 
+                "message": "No such plant exists"
+            }''',
+            content_type="application/json"
+        )
+        response.status_code = 400
+        return response
+
+    plant = dict(plant)
+    plant['_id'] = id
+    data_json = json.dumps(plant)
+    response = HttpResponse(
+        '''{
+            "success": true, 
+            "message": "Here is your plant",
+            "data": ''' + str(data_json) + '''
+        }''',
+        content_type="application/json"
+    )
+    response.status_code = 200
+    return response

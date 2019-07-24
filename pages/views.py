27490484ff6b91442ac5f8f0b90d30a7baad8fbc
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2019-07-24 23:52:14 +0200

    feat: user signup and longin template

--- pages/views.py
+++ pages/views.py
@@ -14,7 +14,7 @@ def createPlant(request):
     if request.method != "POST" or request.body == None:
         response  = HttpResponse(
             '''{
-                "success": false, 
+                "success": false,
                 "message": "Only POST requests are allowed on this route"
             }''',
             content_type="application/json"
@@ -23,10 +23,10 @@ def createPlant(request):
         return response
     
     body = json.loads(request.body)
-    Plant.objects.mongo_insert(body)   
+    Plant.objects.mongo_insert(body)
     response = HttpResponse(
         '''{
-            "success": true, 
+            "success": true,
             "message": "Plant inserted successfully"
         }''',
         content_type="application/json"
@@ -52,9 +52,9 @@ def retrievePlant(request, id):
     plant = Plant.objects.mongo_find_one({'_id': ObjectId(id)})
     # Plant not found
     if plant == None:
-        response  = HttpResponse(   
+        response  = HttpResponse(
             '''{
-                "success": false, 
+                "success": false,
                 "message": "No such plant exists"
             }''',
             content_type="application/json"
@@ -67,8 +67,85 @@ def retrievePlant(request, id):
     data_json = json.dumps(plant)
     response = HttpResponse(
         '''{
-            "success": true, 
+            "success": true,
             "message": "Here is your plant",
+            "data": ''' + str(data_json) + '''
+        }''',
+        content_type="application/json"
+    )
+    response.status_code = 200
+    return response
+
+@csrf_exempt
+def signUp(request):
+    # Only listen to POST requests
+    if request.method != "POST" or request.body == None:
+        response  = HttpResponse(
+            '''{
+                "success": false,
+                "message": "Only POST requests are allowed on this route"
+            }''',
+            content_type="application/json"
+        )
+        response.status_code = 300
+        return response
+    
+    body = json.loads(request.body)
+    # TODO: ENCRYPT PASSWORD HERE
+    # TODO: OTHER AUTH-RELATED FUNCTION IF ANY
+    User.objects.mongo_insert(body)
+    response = HttpResponse(
+        '''{
+            "success": true,
+            "message": "Account created successfully"
+        }''',
+        content_type="application/json"
+    )
+    response.status_code = 200
+    return response
+
+@csrf_exempt
+def logIn(request):
+    # Only listen to GET requests
+    if request.method != "POST":
+        response  = HttpResponse(
+            '''{
+                "success": false, 
+                "message": "Only POST requests are allowed on this route"
+            }''',
+            content_type="application/json"
+        )
+        response.status_code = 300
+        return response
+
+    body = json.loads(request.body)
+    # TODO: ENCRYPT PASSWORD HERE
+    # TODO: OTHER AUTH-RELATED FUNCTION IF ANY
+    user = User.objects.mongo_find_one({
+        'email': body['email'],
+        'password_salt': body['password_salt']
+    })
+    # user not found
+    if user == None:
+        response  = HttpResponse(
+            '''{
+                "success": false,
+                "message": "No such user exists. Sign up or try using another email/password."
+            }''',
+            content_type="application/json"
+        )
+        response.status_code = 400
+        return response
+
+    user = dict(user)
+    # TODO: Don't delete the _id key, turn it to a string of characters
+    del user['_id']
+    data_json = json.dumps(user)
+    # TODO: START SESSION OR ADD TOKEN HERE
+    response = HttpResponse(
+        '''{
+            "success": true,
+            "message": "You have successfully logged in.",
             "data": ''' + str(data_json) + '''
         }''',
         content_type="application/json"

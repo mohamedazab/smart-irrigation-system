215eacbb607869e834b0ca66503e8b283dd8aec4
AbdelrahmanKhaledAmer <abdelrahmanka96@gmail.com>
2019-07-26 00:21:30 +0200

    fix: formulate response feat: add to grid (incomplete)

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import createPlant, retrievePlant,signUp,logIn
+from .views import createPlant, retrievePlant, signUp, logIn, addPlantToGrid
 
 urlpatterns = [
     path('api/login', logIn, name='login'),
     path('api/sign-up', signUp, name='sign-up'),
     path('api/plant', createPlant, name='Create Plant'),
-    path('api/plant/<str:id>', retrievePlant, name='Retrieve Plant')
+    path('api/plant/<str:id>', retrievePlant, name='Retrieve Plant'),
+    path('api/user/add', addPlantToGrid, name='Add Plant To Grid')
 ]
--- pages/views.py
+++ pages/views.py
@@ -16,69 +16,44 @@ from bson.objectid import ObjectId
 def createPlant(request):
     # Only listen to POST requests
     if request.method != "POST" or request.body == None:
-        response  = HttpResponse(
-            '''{
-                "success": false,
-                "message": "Only POST requests are allowed on this route"
-            }''',
-            content_type="application/json"
-        )
-        response.status_code = 300
-        return response
+        response_success =  False
+        response_message = "Only POST requests are allowed on this route"
+        response_code = 300
+        return formulateResponse(response_message, response_success, response_code)
     
     body = json.loads(request.body)
     Plant.objects.mongo_insert(body)
-    response = HttpResponse(
-        '''{
-            "success": true,
-            "message": "Plant inserted successfully"
-        }''',
-        content_type="application/json"
-    )
-    response.status_code = 200
-    return response
+    response_success =  True
+    response_message = "Plant inserted succesfully"
+    response_code = 300
+    return formulateResponse(response_message, response_success, response_code)
 
 
 @csrf_exempt
 def retrievePlant(request, id):
     # Only listen to GET requests
     if request.method != "GET":
-        response  = HttpResponse(
-            '''{
-                "success": false, 
-                "message": "Only GET requests are allowed on this route"
-            }''',
-            content_type="application/json"
-        )
-        response.status_code = 300
-        return response
+        response_success =  False
+        response_message = "Only GET requests are allowed on this route"
+        response_code = 300
+        return formulateResponse(response_message, response_success, response_code)
     
     plant = Plant.objects.mongo_find_one({'_id': ObjectId(id)})
     # Plant not found
     if plant == None:
-        response  = HttpResponse(
-            '''{
-                "success": false,
-                "message": "No such plant exists"
-            }''',
-            content_type="application/json"
-        )
-        response.status_code = 400
-        return response
+        response_success =  False
+        response_message = "No such plant exists"
+        response_code = 400
+        return formulateResponse(response_message, response_success, response_code)
 
     plant = dict(plant)
     plant['_id'] = id
-    data_json = json.dumps(plant)
-    response = HttpResponse(
-        '''{
-            "success": true,
-            "message": "Here is your plant",
-            "data": ''' + str(data_json) + '''
-        }''',
-        content_type="application/json"
-    )
-    response.status_code = 200
-    return response
+    response_message = "account created succesfully"
+    response_success = True
+    response_code = 200
+    response_data = plant
+    return formulateResponse(response_message, response_success, response_code, response_data)
+
 
 @csrf_exempt
 def signUp(request):
@@ -91,7 +66,7 @@ def signUp(request):
     if request.method != "POST" or request.body == None:
         response_success =  False
         response_message ="Only POST requests are allowed on this route"
-        response.status_code = 300
+        response_code = 300
         return formulateResponse(response_message, response_success, response_code)
 
     body = json.loads(request.body)
@@ -99,15 +74,13 @@ def signUp(request):
     if not body["password"] or not body["email"]:
         response_success =  False
         response_message ="incomplete data"
-        response.status_code = 300
+        response_code = 300
         return formulateResponse(response_message, response_success, response_code)
-
 
     # ENCRYPT PASSWORD HERE
     body["password"] = make_password(body["password"])
     # OTHER AUTH-RELATED FUNCTION IF ANY
     User.objects.mongo_insert(body)
-    
     
     return formulateResponse(response_message, response_success, response_code)
 
@@ -124,7 +97,7 @@ def logIn(request):
     if request.method != "POST" or request.body == None:
         response_success =  False
         response_message ="Only POST requests are allowed on this route"
-        response.status_code = 300
+        response_code = 300
         return formulateResponse(response_message, response_success, response_code)
     #load body
     body = json.loads(request.body)
@@ -133,26 +106,22 @@ def logIn(request):
     if not body["password"] or not body["email"]:
         response_success =  False
         response_message ="missing login information"
-        response.status_code = 300
+        response_code = 300
         return formulateResponse(response_message, response_success, response_code)
-         
-
+    
     candidate_user = User.objects.mongo_find_one({"email":body["email"]})
     valid_password =  check_password(body["password"], candidate_user["password"])
     if candidate_user is None or not valid_password:
         response_success =  False
         response_message ="Incorrect email or password"
-        response.status_code = 300
+        response_code = 300
     else:
         if not request.session.exists(request.session.session_key):
             request.session.create()
-            request.session["user_email"] = body["email"] 
+            request.session["user_email"] = body["email"]
         else:
             response_message = "already logged in"
-    # Session.objects.all().delete()
-    # request.session.clear()
-
-
+    
     user  = session_validation(request.session.session_key)
     print("user: ",user)
     return formulateResponse(response_message, response_success, response_code) 
@@ -160,7 +129,7 @@ def logIn(request):
 #returns a user for the current session
 def session_validation(session_key):
     
-    session= Session.objects.filter(session_key = session_key)
+    session = Session.objects.filter(session_key = session_key)
     print("the sessions\n", session, type(session))
     if len(session)<1:
         return None
@@ -170,9 +139,40 @@ def session_validation(session_key):
     return session_data['user_email']
 
 
-def formulateResponse(message, success, code):
-    resp_dict = {"success": success, "message": message}
+@csrf_exempt
+def addPlantToGrid(request):
+    # Only listen to PUT requests
+    if request.method != "PUT" or request.body == None:
+        response_success = False
+        response_message = "Only PUT requests are allowed on this route"
+        response_code = 300
+        return formulateResponse(response_message, response_success, response_code)
 
+    # TODO: Check if logged in.
+    # TODO: Get USER from session.
+
+    plant = Plant.objects.mongo_find_one({'name': request.body['plant_name']})
+    # Plant not found
+    if plant == None:
+        response_success = False
+        response_message = "No such plant exists"
+        response_code = 300
+        return formulateResponse(response_message, response_success, response_code)
+
+    # TODO: Add plant to grid
+    # TODO: Upsert/Update user in collection
+    response_success = True
+    response_message = "User updated successfully"
+    response_code = 200
+    return formulateResponse(response_message, response_success, response_code)
+
+
+def formulateResponse(message, success, code, data=None):
+    if data is None:
+        resp_dict = {"success": success, "message": message}
+    else:
+        resp_dict = {"success": success, "message": message, "data": data}
+    
     response = HttpResponse(
        json.dumps(resp_dict),
         content_type="application/json"
